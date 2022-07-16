18987d7512d1745bc98c0500323bbe7983b874e1
Carlos Aguilera <18aguilerac@gmail.com>
2022-07-16 06:17:25 -0700

    started front end using vuetify

---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col sm="4" class="pa-3" v-for="posting in postings" :key="posting._id">
+        <v-card class="pa-1">
+          <v-img height="250" :src="`/${posting.image}`"></v-img>
+          <v-btn class="ml-4 mt-3" small outlined color="indigo">
+            {{ posting.category }}
+          </v-btn>
+          <v-card-title class="headline">
+            {{ posting.title }}
+          </v-card-title>
+          <v-card-text class="py-0">
+            <p>{{ posting.content.substring(0, 100) + "..." }}</p>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import API from "../api";
+
+  export default {
+    name: 'Home',
+    data() {
+      return {
+        postings: [],
+      };
+    },
+    async created() {
+      this.postings = await API.getAllPostings();
+    }
+  }
+</script>
